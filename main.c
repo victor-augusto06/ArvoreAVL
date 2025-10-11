168acf078c857fe1a7c75b5f61e5c2e7f30cff5f
Victor Augusto <vitor.iac20166@gmail.com>
2025-10-11 11:12:29 -0300

    Inclusão da versão inicial da inserção, ainda sem balanceamento. Inclusão da chamada da função inserção na main e ajustes de chaveamento nos cases

--- main.c
+++ main.c
@@ -9,6 +9,8 @@ typedef struct no{
 	struct no *direita;
 } No;
 
+
+
 void preOrdem(No *raiz){
 	if(raiz==NULL){
 		return;
@@ -36,10 +38,35 @@ void simetrico(No *raiz){
 	simetrico(raiz->direita);	
 }
 
+No* cria_novo_no(int valor){
+	No *novo = (No *)malloc(sizeof(No));
+	novo->noValor=valor;
+	novo->esquerda=NULL;
+	novo->direita=NULL;
+	novo->altura=1;
+	
+	return novo;
+}
+
+No* inserir(No* no_atual, int valor){
+	if(no_atual==NULL){
+		return cria_novo_no(valor);
+	}
+	else{
+		if(no_atual->noValor>valor){
+			no_atual->esquerda=inserir(no_atual->esquerda, valor);
+		}
+		else if(no_atual->noValor<valor){
+			no_atual->direita=inserir(no_atual->direita, valor);
+		}
+	}
+	return no_atual;
+}
+
 int main(){
 	int condicao=1;
 	No *raiz=NULL;
-	
+
 	do{
 		printf("Inicio do programa\n");
 		printf("Digite '1' para inserir elemento na arvore\n");
@@ -50,44 +77,59 @@ int main(){
 		printf("Digite '6' para exibir em formato de Grafos\n");
 		printf("Digite '7' para buscar um elemento x na arvore\n");
 		printf("Digite '8' para editar um elemento x da arvore\n");
-		printf("Digite '9' para balancear a arvore\n");
 		printf("Digite '0' para sair do programa\n");
 		scanf("%d", &condicao);
 		
 		switch(condicao){
 			
-			case 0:
-				return 0;	
-			break;
-			case 1:
+			case 0:{
+				return 0;
 				break;
-			case 2:
+			}
+
+			case 1: {
+				int valor_novo;
+				printf("Digite o valor que deseja inserir: \n");
+				scanf("%d", &valor_novo);
+				raiz = inserir(raiz, valor_novo);
+				printf("%d foi inserido com sucesso!\n\n", valor_novo);
 				break;
+			}
+
+			case 2:{
+				
+				break;
+			}
 			
-			case 3:
-				printf("Iniciando a exibição em modo Pre-Ordem!\n");
+			case 3:{
+				printf("Iniciando a exibicao em modo Pre-Ordem!\n");
 				preOrdem(raiz);
-			break;
+				break;
+			}
 			
-			case 4:
-				printf("Iniciando a exibição em modo Pos-Ordem!\n");
+			case 4:{
+				printf("Iniciando a exibicao em modo Pos-Ordem!\n");
 				posOrdem(raiz);	
-			break;
+				break;
+			}
 			
-			case 5:
-				printf("Iniciando a exibição em modo Simetrico!\n");
-				simetrico(raiz);		
-			break;
+			case 5:{
+				printf("Iniciando a exibicao em modo Simetrico!\n");
+				simetrico(raiz);
+				break;
+			}			
 			
-			case 6:
+			case 6:{
 				break;
-			case 7:
+			}
+				
+			case 7:{
 				break;
-			case 8:
+			}
+			
+			case 8:{
 				break;
-			case 9:
-				break;
-		
+			}
 		}
 		
 	}while(condicao!=0);
